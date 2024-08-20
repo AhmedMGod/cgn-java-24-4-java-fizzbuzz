d19683314f2583211bd04137878297ef92298f9a
AhmedMGod <ahmed_mumin.god@outlook.de>
2024-08-20 10:01:21 +0200

    Add Fizzbuzz method and test

---
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @Test
+    void play_shouldReturn1_whenCalledWith1(){
+        // GIVEN
+        int number = 1;
+        String expected = "1";
+        //WHENN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+}
